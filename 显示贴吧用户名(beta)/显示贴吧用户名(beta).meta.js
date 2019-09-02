840d67ce5595057e21e23683a89d3fa11cd70b13
是甜食哇 <54750485+shitianshiwa@users.noreply.github.com>
2019-09-03 02:24:50 +0900

    创建文件夹

---
+++ 显示贴吧用户名(beta)/显示贴吧用户名(beta).meta.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         显示贴吧用户名(beta)
+// @namespace    http://tampermonkey.net/
+// @version      0.22
+// @description  显示主题贴列表,楼层,楼中楼的贴吧用户名，仅支持电脑端贴吧
+// @author       shitianshiwa
+// @include      http*://tieba.baidu.com/p/*
+// @include      http*://tieba.baidu.com/f?*
+// @grant        GM_registerMenuCommand
+// @downloadURL  https://github.com/shitianshiwa/baidu-tieba-userscript
+// ==/UserScript==
