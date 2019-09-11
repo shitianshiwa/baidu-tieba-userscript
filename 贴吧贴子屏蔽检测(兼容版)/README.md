663d349fa6b6e2de8f0142624dde5618138804a5
是甜食哇 <54750485+shitianshiwa@users.noreply.github.com>
2019-09-12 06:47:24 +0900

    更新

--- 贴吧贴子屏蔽检测(兼容版)/README.md
+++ 贴吧贴子屏蔽检测(兼容版)/README.md
@@ -3,6 +3,8 @@
 # 安装
 - [Install From GitHub](https://github.com/shitianshiwa/baidu-tieba-userscript/raw/master/%E8%B4%B4%E5%90%A7%E8%B4%B4%E5%AD%90%E5%B1%8F%E8%94%BD%E6%A3%80%E6%B5%8B(%E5%85%BC%E5%AE%B9%E7%89%88)/%E8%B4%B4%E5%90%A7%E8%B4%B4%E5%AD%90%E5%B1%8F%E8%94%BD%E6%A3%80%E6%B5%8B(%E5%85%BC%E5%AE%B9%E7%89%88).user.js)
 # 说明
+* 测试中
+* 是改自其它项目的
 * 可能支持无用户名的贴吧账号（楼中楼未完全验证过）
 * 修改为只在各个贴吧的主题列表和主题贴内运行
 * 发主题贴或回贴后，屏蔽样式会消失，刷新贴吧即可
--- 贴吧贴子屏蔽检测(兼容版)/贴吧贴子屏蔽检测(兼容版).user.js
+++ 贴吧贴子屏蔽检测(兼容版)/贴吧贴子屏蔽检测(兼容版).user.js
@@ -6,10 +6,11 @@
 // @include     http*://tieba.baidu.com/f?*
 // @grant       none
 // @license     GPL-3.0
-// @author      shitianshiwa,864907600cc(原项目)
-// @namespace   https://github.com/FirefoxBar/userscript/raw/master/Tieba_Blocked_Detect/Tieba_Blocked_Detect.user.js
-// @downloadURL  https://github.com/shitianshiwa/baidu-tieba-userscript/
+// @author      shitianshiwa,864907600cc(原项目作者)
+// @namespace   https://github.com/FirefoxBar/userscript/tree/master/Tieba_Blocked_Detect
+// @downloadURL  https://github.com/shitianshiwa/baidu-tieba-userscript/tree/master/%E8%B4%B4%E5%90%A7%E8%B4%B4%E5%AD%90%E5%B1%8F%E8%94%BD%E6%A3%80%E6%B5%8B(%E5%85%BC%E5%AE%B9%E7%89%88)
 // ==/UserScript==
+
 //可能支持无用户名的贴吧账号（楼中楼未完全验证过）
 //修改为只在各个贴吧的主题列表和主题贴内运行
 //发主题贴或回贴后，屏蔽样式会消失，刷新贴吧即可
