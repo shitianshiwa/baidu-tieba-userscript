951464b742a9ee37cc60099c3473823bc3e9efdc
是甜食哇 <54750485+shitianshiwa@users.noreply.github.com>
2019-11-05 04:10:23 +0900

    改文件名

--- 贴吧全能助手/贴吧全能助手.user.js
+++ 贴吧全能助手/贴吧全能助手.user.js
@@ -4,9 +4,9 @@
 // @version      2.1(0.01beta)
 // @description  【装这一个脚本就够了～可能是你遇到的最好用的贴吧增强脚本】，百度贴吧 tieba.baidu.com 看贴（包括楼中楼）无须登录，完全去除扰眼和各类广告模块，全面精简并美化各种贴吧页面，去除贴吧帖子里链接的跳转，按发帖时间排序，查看贴吧用户发言记录，贴子关键字屏蔽，移除会员彩名，直接在当前页面查看原图，可缩放，可多开，可拖拽
 // @author       忆世萧遥
-// @include      *://tieba.baidu.com/*
-// @exclude      *://tieba.baidu.com/f/fdir*
-// @exclude      *://tieba.baidu.com/f/search*
+// @include      http*://tieba.baidu.com/*
+// @exclude      http*://tieba.baidu.com/f/fdir*
+// @exclude      http*://tieba.baidu.com/f/search*
 // @run-at       document-body
 ///document-idle
 /// jQuery 留一份自己用
