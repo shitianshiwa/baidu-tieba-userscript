febe1f18ed963da064faea369459f1a8980b084b
是甜食哇 <54750485+shitianshiwa@users.noreply.github.com>
2020-01-06 16:57:50 +0900

    Update Copy Tieba Link.user.js
    完善复制说明

--- Copy Tieba Link/Copy Tieba Link.user.js
+++ Copy Tieba Link/Copy Tieba Link.user.js
@@ -55,25 +55,25 @@ function copyLink() {
     else {
         switch (this.dataset.anchorType) {
             case '0': // 贴吧主题贴列表获取贴子链接
-                if (setting.title) textGroup.push(parent.getElementsByClassName('j_th_tit')[0].getAttribute('title'));
+                if (setting.title) textGroup.push("标题:" + parent.getElementsByClassName('j_th_tit')[0].getAttribute('title'));
                 if (setting.author) textGroup.push((setting.with_at ? '楼主:@' : '楼主:') + JSON.parse(parent.nextElementSibling.getElementsByClassName('j_user_card')[0].getAttribute("data-field")).un + ' ');
                 //parent.nextElementSibling.getElementsByClassName('j_user_card')[0].textContent//旧的复制用户名，会复制昵称
-                if (setting.link) textGroup.push(parent.getElementsByClassName('j_th_tit')[0].href);
+                if (setting.link) textGroup.push("链接：" + parent.getElementsByClassName('j_th_tit')[0].href);
                 break;
             case '1': // 贴子内页获取贴子链接
-                if (setting.title) textGroup.push(unsafeWindow.PageData.thread.title);
+                if (setting.title) textGroup.push("标题:" + unsafeWindow.PageData.thread.title);
                 if (setting.author) textGroup.push((setting.with_at ? '楼主:@' : '楼主:') + unsafeWindow.PageData.thread.author + ' ');
-                if (setting.link) textGroup.push(linkPath + unsafeWindow.PageData.thread.thread_id);
+                if (setting.link) textGroup.push("链接：" + linkPath + unsafeWindow.PageData.thread.thread_id);
                 break;
             case '2': // 贴子内页获取楼层链接
                 //获取楼层的内容
                 var floorData00 = parent.parentNode.parentNode.children[0].children[1].children[1] || parent.parentNode.parentNode.children[0].children[3].children[1] || parent.parentNode.parentNode.parentNode.children[1].children[0].children[3].children[1];
 
                 var floorData = JSON.parse(parent.parentElement.parentElement.parentElement.dataset.field);
-                if (setting.title) textGroup.push(unsafeWindow.PageData.thread.title + ' #' + floorData.content.post_no);
+                if (setting.title) textGroup.push("标题:" + unsafeWindow.PageData.thread.title + ' #' + floorData.content.post_no);
                 if (setting.author) textGroup.push((setting.with_at ? '层主:@' : '层主:') + floorData.author.user_name + ' ');
                 if (setting.neirong_l) textGroup.push("内容:" + floorData00.innerHTML);
-                if (setting.link) textGroup.push(linkPath + unsafeWindow.PageData.thread.thread_id + '?pid=' + floorData.content.post_id + '#' + floorData.content.post_id);
+                if (setting.link) textGroup.push("链接：" + linkPath + unsafeWindow.PageData.thread.thread_id + '?pid=' + floorData.content.post_id + '#' + floorData.content.post_id);
                 break;
             case '3': // 贴子楼中楼获取链接
                 //获取楼层pid、楼层数 兼容http和https的贴子
@@ -84,10 +84,10 @@ function copyLink() {
 
                 var floorData2 = JSON.parse(parent.parentNode.parentNode.getAttribute("data-field").replace(/'/g, '"')); //spid JSON.parse(parent.parentElement.parentElement.parentElement.dataset.field);
                 var floorData3 = JSON.parse(floorData0.replace(/'/g, '"')); //楼层pid
-                if (setting.title) textGroup.push(unsafeWindow.PageData.thread.title + ' #' + floorData1.floor_num + ' 楼中楼');
+                if (setting.title) textGroup.push("标题:" + unsafeWindow.PageData.thread.title + ' #' + floorData1.floor_num + ' 楼中楼');
                 if (setting.author) textGroup.push((setting.with_at ? '回复人:@' : '回复人:') + floorData2.user_name + ' ');
                 if (setting.neirong_lzl) textGroup.push("内容:" + parent.parentNode.children[2].innerHTML);
-                if (setting.link) textGroup.push(linkPath + unsafeWindow.PageData.thread.thread_id + '?pid=' + floorData3.pid + "&cid=" + floorData2.spid + '#' + floorData2.spid);
+                if (setting.link) textGroup.push("链接：" + linkPath + unsafeWindow.PageData.thread.thread_id + '?pid=' + floorData3.pid + "&cid=" + floorData2.spid + '#' + floorData2.spid);
                 //贴吧自带的楼中楼回复定位只能定到楼层那里，楼中楼的回复具体位置要自己去找
                 //console.log(parent.parentNode.parentNode.parentNode);
                 //console.log(parent.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode);
