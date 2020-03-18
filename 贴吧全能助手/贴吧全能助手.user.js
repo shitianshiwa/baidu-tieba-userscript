24f301352e334b7051588ac1bfae651ab597db5a
是甜食哇 <54750485+shitianshiwa@users.noreply.github.com>
2020-03-19 01:13:16 +0900

    Update 贴吧全能助手.user.js
    对
    https://greasyfork.org/ja/scripts/33145-%E8%B4%B4%E5%90%A7%E5%8A%A9%E6%89%8B-%E5%B1%8F%E8%94%BD-%E6%8E%92%E5%BA%8F-beta
    贴吧助手(屏蔽，排序) beta
    （1, 屏蔽指定用户的所有发帖 2 ,排序(按回复))，具体看下面仔细描述）
    进行兼容

--- 贴吧全能助手/贴吧全能助手.user.js
+++ 贴吧全能助手/贴吧全能助手.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         贴吧全能助手(第三方修改)
 // @namespace    http://tampermonkey.net/
-// @version      2.1(0.0163beta)
+// @version      2.1(0.0164beta)
 // @description  【装这一个脚本就够了～可能是你遇到的最好用的贴吧增强脚本】，百度贴吧 tieba.baidu.com 看贴（包括楼中楼）无须登录，完全去除扰眼和各类广告模块，全面精简并美化各种贴吧页面，去除贴吧帖子里链接的跳转，按发帖时间排序，查看贴吧用户发言记录，贴子关键字屏蔽，移除会员彩名，直接在当前页面查看原图，可缩放，可多开，可拖拽
 // @author       忆世萧遥
 // @include      http*://tieba.baidu.com/*
@@ -1176,9 +1176,9 @@ http://tieba.baidu.com/i/i/storethread 使用https链接有bug。原来是http�
                 "	box-sizing: border-box;",
                 "	padding: 6px 10px !important;",
                 "	width: 18% !important;",
-                "	min-width: 240px !important;",
+                "	min-width: 200px !important;",
                 "	position: relative;",
-                "	 margin-left: 12% !important;",
+                //"	 margin-left: 12% !important;",
                 "}",
                 ".search_internal_wrap.pull_right{",
                 "	display: flex;",
