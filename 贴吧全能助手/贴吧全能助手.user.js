6e456be3c298b01ba52b0f162bd48311547b8352
是甜食哇 <54750485+shitianshiwa@users.noreply.github.com>
2020-03-22 09:07:46 +0900

    Update 贴吧全能助手.user.js
    显示主题贴列表里的主题贴创建时间。备注：贴吧自带的创建日期，缺失年或日

--- 贴吧全能助手/贴吧全能助手.user.js
+++ 贴吧全能助手/贴吧全能助手.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         贴吧全能助手(第三方修改)
 // @namespace    http://tampermonkey.net/
-// @version      2.1(0.0164beta)
+// @version      2.1(0.0165beta)
 // @description  【装这一个脚本就够了～可能是你遇到的最好用的贴吧增强脚本】，百度贴吧 tieba.baidu.com 看贴（包括楼中楼）无须登录，完全去除扰眼和各类广告模块，全面精简并美化各种贴吧页面，去除贴吧帖子里链接的跳转，按发帖时间排序，查看贴吧用户发言记录，贴子关键字屏蔽，移除会员彩名，直接在当前页面查看原图，可缩放，可多开，可拖拽
 // @author       忆世萧遥
 // @include      http*://tieba.baidu.com/*
@@ -9143,6 +9143,14 @@ a.jx, .ptr	{ cursor: pointer		}
                 if (temp.length > 0) {
                     for (i = 0; i < temp.length; i++) {
                         temp[i].style = "white-space:normal;";
+                    }
+                }
+            }
+            temp = $("span.is_show_create_time"); //显示主题贴列表里的主题贴创建时间。备注：贴吧自带的创建日期，缺失年或日
+            if (temp.length > 0) {
+                if (temp.length > 0) {
+                    for (i = 0; i < temp.length; i++) {
+                        temp[i].style = "position: relative;display: block;top: -20px;right: 10px;";
                     }
                 }
             }
