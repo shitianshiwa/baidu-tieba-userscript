99572dc857fc0020fa96cfd96df11c80c6ae229b
是甜食哇 <54750485+shitianshiwa@users.noreply.github.com>
2020-05-13 17:21:38 +0900

    Update Kill TieBa AD(非官方修改).user.js

--- Kill TieBa AD(非官方修改)/Kill TieBa AD(非官方修改).user.js
+++ Kill TieBa AD(非官方修改)/Kill TieBa AD(非官方修改).user.js
@@ -3,7 +3,7 @@
 // @name:zh-CN   貼吧廣告清理(非官方修改)
 // @name:zh-TW   貼吧廣告去除(非官方修改)
 // @namespace    hoothin
-// @version      1.3.2((beta)0.323)
+// @version      1.3.2((beta)0.324)
 // @description        Just Kill TieBa AD
 // @description:zh-CN  清理百度貼吧（tieba.baidu.com）内的列表僞裝，帖内僞裝，觸點廣告，頁首廣告，推薦應用等各類廣告
 // @description:zh-TW  去除百度貼吧（tieba.baidu.com）内的列表偽裝，帖內偽裝，觸點廣告，頁首廣告，推薦應用等各類廣告
@@ -12,7 +12,7 @@
 // @include      http*://tieba.baidu.com/f?*
 // @exclude      http*://tieba.baidu.com/
 // @exclude      http*://tieba.baidu.com/index.html
-// @run-at       document-idle
+// @run-at       document-body
 // @grant        none
 // @supportURL   http://www.hoothin.com
 // @license     MIT License
@@ -103,21 +103,20 @@
             }
             //class="l_banner banner_theme"
 
-            /*easyADs=document.querySelectorAll("#banner_pb_customize");//贴子内的吧背景图顶偶然发现的广告,这个出现一段时间后会自己消失
+            easyADs = document.querySelectorAll("#banner_pb_customize"); //贴子内的吧背景图顶偶然发现的广告,这个出现一段时间后会自己消失
             //alert(easyADs[0])
-            if(easyADs[0]!=null)
-            {
-                easyADs[0].style="display:none;";
-              //easyADs[0].remove();
-            }*/
+            if (easyADs[0] != null) {
+                easyADs[0].style = "display:none;";
+                //easyADs[0].remove();
+            }
 
-            easyADs = $("div.cps_wrap") //主题贴内右边贴吧热议榜下面的广告
+            /*easyADs = $("div.cps_wrap") //主题贴内右边贴吧热议榜下面的广告
             if (easyADs.children("span.media_ad")[0] != null) {
                 if (easyADs.children("span.media_ad")[0].innerHTML == "广告" || "廣告") {
                     easyADs.children("img.cps_img")[0].style = "display:none;"; //删掉似乎会影响贴子内容加载，所以用样式隐藏起来,这个出现一段时间后会自己消失
                     easyADs.children("span.media_ad")[0].style = "display:none;";
                 }
-            }
+            }*/
 
             easyADs = document.querySelectorAll("span.media_ad");
             if (easyADs[0] != null) {
@@ -131,7 +130,8 @@
                 easyADs[0].style = "display:none;";
             }
         } catch (error) {
-            alert(error);
+            console.log(error);
+            //alert(error);
         }
 
         function delAD(a, b) //会执行多次
