632d5e699bfd64bfc4c1d460e6607c8df41a6371
是甜食哇 <54750485+shitianshiwa@users.noreply.github.com>
2020-06-18 23:40:15 +0900

    Update 贴吧全能助手.user.js
    提高右上角按钮显示头像的成功率？

--- 贴吧全能助手/贴吧全能助手.user.js
+++ 贴吧全能助手/贴吧全能助手.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         贴吧全能助手(第三方修改)
 // @namespace    http://tampermonkey.net/
-// @version      2.1(0.016948beta)
+// @version      2.1(0.016949beta)
 // @description  【装这一个脚本就够了～可能是你遇到的最好用的贴吧增强脚本】，百度贴吧 tieba.baidu.com 看贴（包括楼中楼）无须登录，完全去除扰眼和各类广告模块，全面精简并美化各种贴吧页面，去除贴吧帖子里链接的跳转（已失效），按发帖时间排序，查看贴吧用户发言记录，贴子关键字屏蔽，移除会员彩名，直接在当前页面查看原图，可缩放，可多开，可拖拽
 // @author       忆世萧遥,shitianshiwa
 // @include      http*://tieba.baidu.com/*
@@ -9814,8 +9814,9 @@ background-image: url(http://onox.qiniudn.com/maverick/tbbg/1.jpg) !important;
             if (classList.contains('u_menu_username')) {
                 //console.log(target);
                 // @returns {number|""} 是否登录，不登陆为0或"",为了适配不登陆看贴功能
+                //console.log(localStorage.getItem("userid"));
                 var getIsLogin2 = unsafeWindow.PageData.user.id || unsafeWindow.PageData.user.user_id; //获取用户id
-                if (localStorage.getItem("userid") == null) {
+                if (localStorage.getItem("userid") == null||localStorage.getItem("userid")!= ""||localStorage.getItem("userid")!=undefined) {//可能没用？
                     localStorage.setItem("userid", getIsLogin2)
                 }
                 //console.log(getIsLogin2)
@@ -9836,7 +9837,7 @@ background-image: url(http://onox.qiniudn.com/maverick/tbbg/1.jpg) !important;
                         } else {
                             temp = $("img.head_img")[0] || $("a.userinfo_head>img")[0] || $("#img_aside_head")[0] || $("span.pm_user_logo>img")[0] || $("img.user_avatar")[0];
                             //贴吧主题列表，我的贴吧，我的i贴吧，贴吧服务中心，吧务后台
-                            console.log(temp.src)
+                            //console.log(temp.src)
                             localStorage.setItem("userimg", temp.src)
                             userimg = temp.src;
                         }
