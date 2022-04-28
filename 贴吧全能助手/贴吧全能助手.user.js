d5b5f17fb6e0e0bb73c571e068e5ca6cb6fd8a2b
shitianshiwa <54750485+shitianshiwa@users.noreply.github.com>
2022-04-29 08:34:34 +0900

    Update 贴吧全能助手.user.js

--- 贴吧全能助手/贴吧全能助手.user.js
+++ 贴吧全能助手/贴吧全能助手.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         贴吧全能助手(第三方修改)
 // @namespace    http://tampermonkey.net/
-// @version      2.1.1843
+// @version      2.1.1843.1
 /// @version     2.1
 // @description  【装这一个脚本就够了～可能是你遇到的最好用的贴吧增强脚本】(不存在的)，百度贴吧 tieba.baidu.com 看贴（包括楼中楼）无须登录，完全去除扰眼和各类广告模块(贴吧活动广告不管了，都是针对某个贴吧弄的，来无影去无踪，能证明PC贴吧还有人管。。。)，全面精简并美化各种贴吧页面（算不算好要看个人喜好），去除贴吧帖子里链接的跳转（beta），按发贴时间排序/倒序，查看贴吧用户发言记录（有些用户查不了;已经废了），贴子关键字屏蔽（作用不大），移除会员彩名，直接在当前页面查看原图，可缩放，可多开，可拖拽
 // @author       shitianshiwa && 忆世萧遥
@@ -10744,7 +10744,12 @@ margin-top: 20px;
                                     }
                                 }, "json");
                         } else {
-                            userimg = "https://himg.bdimg.com/sys/portrait/item/" + userportrait;
+                            if (userportrait.search("http") == -1) {
+                                userimg = "https://himg.bdimg.com/sys/portrait/item/" + userportrait
+                            }
+                            else {
+                                userimg = userportrait//有些页面是完整的链接，例如贴吧热议https://tieba.baidu.com/hottopic/browse/hottopic?topic_id=xxxxxx&topic_name=xxxxx
+                            }
                         }
                         /*
                         贴吧用户头像
